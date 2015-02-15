ab2ec4983eb3977a7b82f8c7b5d58a66be93b24c
Gustavo Maloste <gustavomaloste@gmail.com>
2015-02-15 19:44:14 -0200

    MergeSort

--- Algorithms.Tests/Sort/MergeSortTest.cs
+++ Algorithms.Tests/Sort/MergeSortTest.cs
@@ -8,23 +8,30 @@ using System.Text;
 namespace Algorithms.Tests.Sort
 {
     [TestClass]
-    public class QuickSortTest
+    public class MergeSortTest
     {
+        [TestMethod]
+        public void ShouldSortTwo()
+        {
+            var input = new[] { 1,0 };
+            MergeSort.Sort(input);
+            Assert.IsTrue(new[] { 0,1 }.SequenceEqual(input));
+        }
+        
         [TestMethod]
         public void ShouldSort()
         {
-            var input = new[] { 5,7,8,1,2,6 };
-            QuickSort.Sort(input);
-            Assert.IsTrue(new[] { 1, 2, 5,6, 7, 8 }.SequenceEqual(input));
+            var input = new[] { 5, 7, 8, 1, 2, 6 };
+            MergeSort.Sort(input);
+            Assert.IsTrue(new[] { 1, 2, 5, 6, 7, 8 }.SequenceEqual(input));
         }
 
         [TestMethod]
         public void ShouldSortWithDuplicates()
         {
             var input = new[] { 5, 7, 8, 1, 2, 6, 6, 6, 8, 0 };
-            QuickSort.Sort(input);
+            MergeSort.Sort(input);
             Assert.IsTrue(new[] { 0, 1, 2, 5, 6, 6, 6, 7, 8, 8 }.SequenceEqual(input));
         }
-
     }
 }
---
+++ Algorithms/Sort/MergeSort.cs
@@ -0,0 +1,52 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+
+namespace Algorithms.Sort
+{
+    public class MergeSort
+    {
+        public static void Sort(int[] arr)
+        {
+            DivideAndMerge(arr, 0, arr.Length - 1);
+        }
+
+        public static void DivideAndMerge(int[] arr, int iStart, int iEnd)
+        {
+            if (iEnd - iStart <= 1)
+            {
+                if (iEnd - iStart == 1)
+                {
+                    var merged = Merge(arr, iStart, iEnd, iEnd);
+                    Array.Copy(merged, 0, arr, iStart, merged.Length);
+                }
+                return;
+            }
+            var middle = (int)(iEnd + iStart)/2;
+            DivideAndMerge(arr, iStart, middle);
+            DivideAndMerge(arr, middle, iEnd);
+            var _merged = Merge(arr, iStart, middle, iEnd);
+            Array.Copy(_merged, 0, arr, iStart, _merged.Length);
+        }
+
+        private static int[] Merge(int[] a, int iStart, int iCenter, int iEnd)
+        {
+            int iRunA = iStart,
+                iRunB = iCenter;
+            var result = new int[iEnd - iStart + 1];
+            for (var iRun = 0; iRun < result.Length; iRun++)
+            {
+                if (iRunB > iEnd || (iRunA < iCenter && a[iRunA] <= a[iRunB]))
+                {
+                    result[iRun] = a[iRunA++];
+                }
+                else
+                {
+                    result[iRun] = a[iRunB++];
+                }
+            }
+            return result;
+        }
+    }
+}
