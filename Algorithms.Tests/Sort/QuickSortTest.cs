19d8aa70d0b778ba649afb6ca099298f0ea3813d
Gustavo Maloste <gustavomaloste@gmail.com>
2015-02-15 12:15:37 -0200

    QuickSort and test

---
+++ Algorithms.Tests/Sort/QuickSortTest.cs
@@ -0,0 +1,30 @@
+﻿using Algorithms.Sort;
+using Microsoft.VisualStudio.TestTools.UnitTesting;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+
+namespace Algorithms.Tests.Sort
+{
+    [TestClass]
+    public class QuickSortTest
+    {
+        [TestMethod]
+        public void ShouldSort()
+        {
+            var input = new[] { 5,7,8,1,2 };
+            QuickSort.Sort(input);
+            Assert.IsTrue(new[] { 1, 2, 5, 7, 8 }.SequenceEqual(input));
+        }
+
+        [TestMethod]
+        public void ShouldSortWithDuplicates()
+        {
+            var input = new[] { 5, 7, 8, 1, 2, 6, 6, 6, 8, 0 };
+            QuickSort.Sort(input);
+            Assert.IsTrue(new[] { 0, 1, 2, 5, 6, 6, 6, 7, 8, 8 }.SequenceEqual(input));
+        }
+
+    }
+}
---
+++ Algorithms/Sort/QuickSort.cs
@@ -0,0 +1,45 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+
+namespace Algorithms.Sort
+{
+    public class QuickSort
+    {
+        private static void Sort(int[] input, int lowerIndex, int upperIndex)
+        {
+            if (lowerIndex >= upperIndex)
+            {
+                return;
+            }
+            var pivotIndex = (int)((upperIndex - lowerIndex) / 2 + lowerIndex) ;
+            var pivotValue = input[pivotIndex];
+            Swap(input, pivotIndex, upperIndex);
+            pivotIndex = lowerIndex;
+            for (var i = lowerIndex; i < upperIndex; i++)
+            {
+                if (input[i] < pivotValue)
+                {
+                    Swap(input, i, pivotIndex);
+                    pivotIndex++;
+                }
+            }
+            Swap(input, pivotIndex, upperIndex);
+            Sort(input, lowerIndex, pivotIndex - 1);
+            Sort(input, pivotIndex + 1, upperIndex);
+        }
+
+        private static void Swap(int[] input, int a, int b)
+        {
+            var tmp = input[a];
+            input[a] = input[b];
+            input[b] = tmp;
+        }
+
+        public static void Sort(int[] input)
+        {
+            Sort(input, 0, input.Length-1);
+        }
+    }
+}
